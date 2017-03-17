6ae35c9fb3eb76df0a09f9dd36c623f0f24c26f8
huen oh <heons921@gmail.com>
2017-03-17 13:25:20 +0900

    add : Display 2d function in 3d coordinate

--- gradient_2d.py
+++ gradient_2d.py
@@ -2,7 +2,7 @@
 # cf.http://d.hatena.ne.jp/white_wheels/20100327/p3
 import numpy as np
 import matplotlib.pylab as plt
-from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.axes3d import Axes3D
 
 
 def numerical_gradient_2d(f, x, y):
@@ -25,9 +25,26 @@ if __name__ == '__main__':
     x1 = np.arange(-2, 2.5, 0.25)
     X, Y = np.meshgrid(x0, x1)
 
+    # Display function_2
+    Z = function_2(X, Y)
+
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.plot_wireframe(X, Y, Z)
+    plt.show()
+
+    # Display function_3
+    Z = function_3(X, Y)
+
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.plot_surface(X, Y, Z)
+    plt.show()
+
     X = X.flatten()
     Y = Y.flatten()
 
+    # 2d gradient of function_2
     grad_x, grad_y = numerical_gradient_2d(function_2, X, Y)
     plt.figure()
     plt.quiver(X, Y, -grad_x, -grad_y, angles="xy", color="#666666")  # ,headwidth=10,scale=40,color="#444444")
@@ -40,6 +57,7 @@ if __name__ == '__main__':
     plt.draw()
     plt.show()
 
+    # 2d gradient of function_3
     grad_x, grad_y = numerical_gradient_2d(function_3, X, Y)
     plt.figure()
     plt.quiver(X, Y, -grad_x, -grad_y, angles="xy", color="#444444")  # ,headwidth=10,scale=40,color="#444444")
