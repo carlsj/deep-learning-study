f75f87981765cc05a5488eedfb5e5832e08c6784
Huen Oh <heons921@gmail.com>
2017-03-20 13:34:58 +0900

    add : two_layer_net and it's training sample

---
+++ network/train_two_layer_net.py
@@ -0,0 +1,69 @@
+# coding: utf-8
+import matplotlib.pylab as plt
+
+from dataset.mnist.mnist import load_mnist
+from network.two_layer_net import *
+
+sys.path.append(os.pardir)
+
+
+# Load MNIST data
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=True)
+
+# Hyper Parameters
+train_size = x_train.shape[0]  # 60000
+input_size = x_train.shape[1]  # 784
+hidden_size = 50
+output_size = 10
+weight_init_std = 0.01
+iters_num = 10000
+lr = 0.1
+batch_size = 100
+
+# Create Network
+mnist_nn = TwoLayerNet(input_size, hidden_size, output_size, weight_init_std)
+iter_per_epoch = max(train_size / batch_size, 1)
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+# Training
+for ind_iters in range(0, iters_num):
+    #batch_mask = np.random.randint(0, input_size, batch_size)
+    #  - train result is not good as using np.random.choice : why?
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    # Compute gradient
+    #grad = mnist_nn.numerical_gradient(x_batch, t_batch)
+    grad = mnist_nn.gradient(x_batch, t_batch)
+
+    # Update parameters
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        mnist_nn.param[key] -= lr * grad[key]
+
+    # Record loss
+    loss = mnist_nn.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+
+    # Accuracy in every epoch
+    if ind_iters % iter_per_epoch == 0:
+        train_acc = mnist_nn.accuracy(x_train, t_train)
+        train_acc_list.append(train_acc)
+        test_acc = mnist_nn.accuracy(x_test, t_test)
+        test_acc_list.append(test_acc)
+        print("train acc / test acc : " + str(train_acc) + "/" + str(test_acc))
+
+# Plot
+markers = {'train': 'o', 'test': 's'}
+x = np.arange(len(train_acc_list))
+plt.plot(x, train_acc_list, label='train_acc')
+plt.plot(x, test_acc_list, label='test_acc', linestyle='--')
+plt.xlabel('epochs')
+plt.ylabel('accuracy')
+plt.ylim(0, 1.0)
+plt.legend(loc='lower right')
+plt.show()
+
---
+++ network/two_layer_net.py
@@ -0,0 +1,90 @@
+# coding: utf-8
+import sys
+import os
+from common.functions import *
+from common.gradient import numerical_gradient
+
+
+sys.path.append(os.pardir)
+
+
+class TwoLayerNet:
+
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        # Initialize parameters
+        self.param = {}
+        self.param['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.param['b1'] = np.zeros(hidden_size)
+        self.param['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.param['b2'] = np.zeros(output_size)
+
+    def predict(self, x):
+        W1 = self.param['W1']
+        W2 = self.param['W2']
+        b1 = self.param['b1']
+        b2 = self.param['b2']
+
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = sigmoid(a2)
+
+        return y
+
+    def loss(self, x, t):
+        y = self.predict(x)
+        return cross_entropy_err(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+
+        grad = {}
+        grad['W1'] = numerical_gradient(loss_W, self.param['W1'])
+        grad['b1'] = numerical_gradient(loss_W, self.param['b1'])
+        grad['W2'] = numerical_gradient(loss_W, self.param['W2'])
+        grad['b2'] = numerical_gradient(loss_W, self.param['b2'])
+
+        return grad
+
+    def gradient(self, x, t):
+        grad = {}
+        W1 = self.param['W1']
+        W2 = self.param['W2']
+        b1 = self.param['b1']
+        b2 = self.param['b2']
+
+        batch_size = x.shape[0]
+
+        # forward
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        # backward : when t is one-hot-encoding label
+        dy = (y - t) / batch_size
+        grad['W2'] = np.dot(z1.T, dy)
+        grad['b2'] = np.sum(dy, axis=0)
+
+        da1 = np.dot(dy, W2.T)
+        dz1 = sigmoid_grad(a1) * da1
+        grad['W1'] = np.dot(x.T, dz1)
+        grad['b1'] = np.sum(dz1, axis=0)
+
+        return grad
+
+
+
+
+
+
+
+
